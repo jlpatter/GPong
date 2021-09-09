bf2adbee9ca4ba85b7cf42a0c5512bb91dc96b94
Joshua Patterson <jleegippies@gmail.com>
2021-09-09 17:36:54 -0500

    Got midline drawing properly

---
+++ src/MidLine.gd
@@ -0,0 +1,9 @@
+extends Line2D
+
+func _ready():
+	var my_list = PoolVector2Array()
+	my_list.append(Vector2(get_viewport_rect().size.x / 2.0, 0.0))
+	my_list.append(Vector2(get_viewport_rect().size.x / 2.0, get_viewport_rect().size.y))
+	
+	points = my_list
+	print(points)
